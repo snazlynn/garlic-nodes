7889bc49bfba6741343e251e18b9f87d47add2d1
cadermill <cadenrxmiller@gmail.com>
2023-10-25 08:56:10 -0500

    :P

--- Assets/scripts/spawnPlanet.cs
+++ Assets/scripts/spawnPlanet.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class spawnPlanet : MonoBehaviour
 {
+    public bool lastScene = false;
+    public float respawnAngle;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, Random.Range(0, 360));
+        if (name != "Earth")
+            transform.RotateAround(Vector3.zero, Vector3.forward, Random.Range(0, 360));
+        DontDestroyOnLoad(this);
+    }
+
+    void OnEnable()
+    {
+        if (lastScene){
+            transform.RotateAround(Vector3.zero, Vector3.forward, respawnAngle);
+        }
+        lastScene = false;
     }
 }
